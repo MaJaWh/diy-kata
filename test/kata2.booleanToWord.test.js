8c589b07c5f56b8287117cab51ae32f5698c6f42
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-08 20:20:51 +0100

    Completed the first boolean Diy Kata

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns yes when passed true", () => {
+    expect(booleanToWord(true)).toEqual("Yes");
+  });
+
+  it("returns no when passed false", () => {
+    expect(booleanToWord(false)).toEqual("No");
+  });
 });
