c0594ea4bd656692bf8e1bdc8e607f354860023a
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-09 19:49:46 +0100

    Completed reachDestination

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,16 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  const numberFirst = 15;
+  const numberSecond = 9;
+  const numberThird = (number - 2) * 4;
+  const numberFourth = (number - 2) * 5;
+
+  if (number <= 1) {
+    return numberFirst;
+  } else if (number === 2) {
+    return numberFirst + numberSecond;
+  } else {
+    return numberFirst + numberSecond + numberThird;
+  }
+};
 
 module.exports = humanCatDogYears;
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,8 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  // time = distance / speed
+  const time = distance / speed;
+  const newArray = (Math.round(time * 2) / 2).toFixed(1);
+  return `I should be there in ${newArray} hours.`;
+};
 
 module.exports = reachDestination;
