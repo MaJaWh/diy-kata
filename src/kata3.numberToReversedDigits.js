9b59946e23328fb55f4b0c2fc88b5fee3a280892
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-09 17:31:45 +0100

    Completed DIY-Kata 3

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,11 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = (number) => {
+  const myArray = String(number)
+    .split("")
+    .reverse()
+    .map((number) => {
+      return Number(number);
+    });
+  return myArray;
+};
 
 module.exports = numberToReversedDigits;
